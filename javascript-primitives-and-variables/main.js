c79b6ea7639f4beb50dccbf46a5919b51e1f6fcf
Luca Chitayat <lucachitayat@gmail.com>
2023-08-28 18:44:42 +0000

    completed javascript primitives and variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+const fullName = 'Luca Chitayat';
+const isCool = false;
+const totalPets = 0;
+
+console.log('Full name provided: ', fullName);
+console.log('Provided person is cool?  :', isCool);
+console.log('Total pets owned by person: ', totalPets);
+
+console.log('Typeof for fullName: ', typeof fullName);
+console.log('Typeof for  iscool :', typeof isCool);
+console.log('Typeof for totalPets: ', typeof totalPets);
+
+const unknown = null;
+console.log(unknown);
+console.log(typeof unknown);
+
+let never;
+console.log(never);
+console.log(typeof never);
